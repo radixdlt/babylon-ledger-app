da0126f7e9bd36af163284a59b9f25c35a452916
Sergiy Yevtushenko <sergiy@radixdlt.com>
2022-10-06 16:24:57 +0200

    Configure building manifests and loadable hex files

--- prepare-binaries.sh
+++ prepare-binaries.sh
@@ -4,7 +4,12 @@ export LEDGER_TARGETS=./target-config
 
 echo "Building RELEASE for NANO S"
 cargo build --release -Z build-std=core --target=./target-config/nanos.json
+cargo ledger nanos
+
 echo "Building RELEASE for NANO S Plus"
 cargo build --release -Z build-std=core --target=./target-config/nanosplus.json
+cargo ledger nanosplus
+
 echo "Building RELEASE for NANO X"
 cargo build --release -Z build-std=core --target=./target-config/nanox.json
+cargo ledger nanox
