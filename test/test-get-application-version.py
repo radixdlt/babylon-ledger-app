c785365866aa0044e2d9ac90389c5d43edc7b85a
Sergiy Yevtushenko <sergiy@radixdlt.com>
2022-11-01 09:45:51 +0100

    Updated tests

--- test/test-get-application-version.py
+++ test/test-get-application-version.py
@@ -20,5 +20,5 @@ dataLength = "00"
 print("Testing", "GetAppVersion", instructionCode, end=" ")
 response = dongle.exchange(bytes.fromhex(instructionClass + instructionCode + p1 + p2 + dataLength))
 
-assert response.hex() == '000001', "Invalid version"
+assert response.hex() == '000001', "Invalid version\nReceived:" + response.hex()
 print("Success")
--- test/test-get-device-id.py
+++ test/test-get-device-id.py
@@ -20,6 +20,6 @@ dataLength = "00"
 print("Testing", "GetDeviceId", instructionCode, end=" ")
 response = dongle.exchange(bytes.fromhex(instructionClass + instructionCode + p1 + p2 + dataLength))
 
-assert response.hex() == '3cfe24be7239bf8926150744888d1337655027e0ec8705a0be132c150ba042ce', "Invalid device ID"
+assert response.hex() == '305495ba0fdfd3c400568ce7a2f4e4d446f3cd8b305a9d7b43f4e4257d71a248', "Invalid device ID\nReceived:" + response.hex()
 print("Success")
 
