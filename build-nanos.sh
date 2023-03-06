451f222475efc0e7cb17c19ea41c59c255b312e0
Sergiy Yevtushenko <sergiy.yevtushenko@gmail.com>
2023-03-06 13:24:38 +0100

    Add scripts to build/flash Nano S Plus

--- build-nanos.sh
+++ build-nanos.sh
@@ -3,5 +3,5 @@ set -e
 export LEDGER_TARGETS=./target-config
 export CARGO_REGISTRIES_CRATES_IO_PROTOCOL=sparse
 
-echo "Building DEBUG & RELEASE for NANO S"
+echo "Building DEBUG for NANO S"
 cargo build -Z sparse-registry -Z build-std=core -Z build-std-features=compiler-builtins-mem --target=./target-config/nanos.json
