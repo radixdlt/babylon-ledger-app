8f763caaebf7646d6b4bb35e917946c000156c7f
Sergiy Yevtushenko <sergiy.yevtushenko@gmail.com>
2023-04-20 17:15:06 +0200

    Move data back to stack Add visual feedback that app is accessed by host

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@
 
 use nanos_sdk::buttons::ButtonEvent;
 use nanos_sdk::io::{Comm, Event};
-use nanos_ui::ui::SingleMessage;
+use nanos_ui::ui::{SingleMessage, Validator};
 
 use handler::dispatcher;
 
@@ -29,23 +29,39 @@ nanos_sdk::set_panic!(nanos_sdk::exiting_panic);
 
 // Application Name
 const APPLICATION: &str = env!("CARGO_PKG_DESCRIPTION");
+const APPLICATION_FLASH: &str = concat!("*", env!("CARGO_PKG_DESCRIPTION"), "*");
 
 #[no_mangle]
 extern "C" fn sample_main() {
     let mut comm = Comm::new();
     let mut state = TxSignState::new();
+    let mut ticker = 0;
+
+    SingleMessage::new(APPLICATION).show();
 
     loop {
-        SingleMessage::new(APPLICATION).show();
+        let event = comm.next_event();
 
-        match comm.next_event() {
+        match event {
             // Press both buttons to exit app
             Event::Button(ButtonEvent::BothButtonsPress) => nanos_sdk::exit_app(0),
 
-            Event::Command(ins) => match dispatcher::dispatcher(&mut comm, ins, &mut state) {
-                Ok(()) => comm.reply_ok(),
-                Err(app_error) => comm.reply(app_error),
-            },
+            Event::Command(ins) => {
+                ticker = 1; //about 0.1seconds
+                match dispatcher::dispatcher(&mut comm, ins, &mut state) {
+                    Ok(()) => comm.reply_ok(),
+                    Err(app_error) => comm.reply(app_error),
+                }
+            }
+
+            Event::Ticker => {
+                if ticker == 0 {
+                    SingleMessage::new(APPLICATION).show();
+                } else {
+                    ticker -= 1;
+                    SingleMessage::new(APPLICATION_FLASH).show();
+                }
+            }
             _ => (),
         }
     }
