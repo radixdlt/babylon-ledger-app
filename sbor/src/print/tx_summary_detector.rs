abb66e37914c5fd7796d527e0ed417c41c4de72d
Sergiy Yevtushenko <sergiy@radixdlt.com>
2023-09-25 23:18:22 +0200

    More strict simple transfer check
    (cherry picked from commit 6c6e3b30d6901c672aee861ec52dcaf4f3cf8301)

--- sbor/src/print/tx_summary_detector.rs
+++ sbor/src/print/tx_summary_detector.rs
@@ -219,7 +219,9 @@ impl TxSummaryDetector {
             (FeePhase::Start, Instruction::CallMethod) => {
                 self.fee_phase = FeePhase::Address;
             }
-            (_, _) => {}
+            (_, _) => {
+                self.fee_phase = FeePhase::Start;
+            }
         }
 
         if self.intent_type != TxIntentType::Transfer {
@@ -244,8 +246,9 @@ impl TxSummaryDetector {
                     self.decoding_phase = DecodingPhase::NonConformingTransaction;
                 }
             }
-            // TODO: How to reliably detect nonconforming transaction here?
-            (_, _) => {}
+            (_, _) => {
+                self.decoding_phase = DecodingPhase::NonConformingTransaction;
+            }
         }
     }
 
@@ -359,6 +362,8 @@ impl TxSummaryDetector {
                     || self.data.eq(b"try_deposit_or_refund")
                 {
                     self.decoding_phase = DecodingPhase::DoneTransfer;
+                    self.fee_phase = FeePhase::Start;
+                    return;
                 } else {
                     self.decoding_phase = DecodingPhase::NonConformingTransaction;
                 }
@@ -404,6 +409,10 @@ impl TxSummaryDetector {
                     self.fee_phase = FeePhase::ValueStart;
                 } else {
                     self.fee_phase = FeePhase::Start;
+
+                    if self.decoding_phase == DecodingPhase::DoneTransfer {
+                        self.decoding_phase = DecodingPhase::NonConformingTransaction;
+                    }
                 }
             }
             FeePhase::Value => {
