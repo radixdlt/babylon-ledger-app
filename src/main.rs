1bbd0554beaa2aa13f91a505d2ba6ec3c8cee4cc
Sergiy Yevtushenko <sergiy.yevtushenko@rdx.works>
2023-10-13 15:44:52 +0200

    Fix/missing timeout (#54)
    * Enable screen/device lock timeout

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@
 #![feature(cfg_version)]
 #![feature(const_mut_refs)]
 
+use nanos_sdk::bindings::BOLOS_UX_OK;
 use nanos_sdk::io::{Comm, Event};
+use nanos_sdk::uxapp::UxEvent;
 use nanos_ui::bagls::{CERTIFICATE_ICON, COGGLE_ICON, DASHBOARD_X_ICON, PROCESSING_ICON};
 use nanos_ui::ui::clear_screen;
 
@@ -191,6 +193,10 @@ extern "C" fn sample_main() {
                     if ticker == 0 {
                         main_menu.display();
                     }
+                }
+                if UxEvent::Event.request() != BOLOS_UX_OK {
+                    UxEvent::block();
+                    main_menu.display();
                 }
             }
         }
