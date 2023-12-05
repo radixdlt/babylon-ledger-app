7b3bd2dd2e5d87ce2e0a435111632405c0714c6d
Sergiy Yevtushenko <sergiy@radixdlt.com>
2023-12-05 14:27:56 +0100

    Fix formatting

--- src/io.rs
+++ src/io.rs
@@ -1,8 +1,8 @@
+#[cfg(target_os = "nanox")]
+use ledger_secure_sdk_sys::ble;
 use ledger_secure_sdk_sys::buttons::{get_button_event, ButtonEvent, ButtonsState};
 use ledger_secure_sdk_sys::seph as sys_seph;
 use ledger_secure_sdk_sys::*;
-#[cfg(target_os = "nanox")]
-use ledger_secure_sdk_sys::ble;
 
 use core::convert::TryFrom;
 #[cfg(feature = "ccid")]
