26b7be03e73c683a3256510f4eb02ed092206c8b
Sergiy Yevtushenko <sergiy.yevtushenko@gmail.com>
2024-01-29 09:35:52 +0100

    Let ragger skip the test

--- ragger_tests/test_get_app_version.py
+++ ragger_tests/test_get_app_version.py
@@ -1,8 +1,9 @@
-# define the CLA of your application here
+import pytest
+
 CLA = 0xAA
-# define an instruction of your application here
 INS = 0x10
 
 
+@pytest.mark.use_on_backend("ledgercomm")
 def test_get_version(backend):
     print(backend.exchange(cla=CLA, ins=INS))
