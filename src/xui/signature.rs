cc6127fd0ceef6296d5306d71a9a6a10dd30f387
Sergiy Yevtushenko <sergiy.yevtushenko@rdx.works>
2024-09-18 13:47:40 +0200

    Fix UI

--- src/xui/signature.rs
+++ src/xui/signature.rs
@@ -8,10 +8,10 @@ pub enum SignType {
 }
 #[cfg(not(target_os = "stax"))]
 pub fn ask_user(sign_type: SignType) -> bool {
-    let message = match sign_type {
+    let message: &[&str] = match sign_type {
         SignType::TX => &["Sign TX?"],
         SignType::Proof => &["Sign Proof?"],
-        SignType::PreAuthHash => &["Sign Pre-auth?"],
+        SignType::PreAuthHash => &["Sign", "Pre-auth?"],
     };
     MultipageValidator::new(message, &["Sign"], &["Reject"]).ask()
 }
