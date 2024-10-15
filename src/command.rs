0ba18647437052aa3bc9dd5c25279fceaea651c7
Sergiy Yevtushenko <sergiy.yevtushenko@rdx.works>
2024-10-15 14:05:43 +0200

    Adjust command numbering scheme to leave space for further extensions. Only new commands are affected. (#79)

--- src/command.rs
+++ src/command.rs
@@ -39,7 +39,7 @@ impl TryFrom<ApduHeader> for Command {
             0x81 => Ok(Command::VerifyAddressEd25519),
             0x91 => Ok(Command::VerifyAddressSecp256k1),
             0xA1 => Ok(Command::SignPreAuthHashEd25519),
-            0xB1 => Ok(Command::SignPreAuthHashSecp256k1),
+            0xA2 => Ok(Command::SignPreAuthHashSecp256k1),
             _ => Err(AppError::NotImplemented),
         }
     }
