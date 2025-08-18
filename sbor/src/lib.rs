36d7c13a973610120c6db7abec291693952a81af
GroM <jerome.caporossi@ledger.fr>
2025-08-19 00:14:42 +0200

    Fix clippy

--- sbor/src/lib.rs
+++ sbor/src/lib.rs
@@ -56,6 +56,7 @@
 // their parameters.
 
 #![feature(prelude_2024)]
+#![feature(const_mut_refs)]
 #![feature(generic_const_exprs)]
 #![allow(incomplete_features)]
 #![cfg_attr(not(test), no_std)]
--- src/sign/tx_state.rs
+++ src/sign/tx_state.rs
@@ -274,9 +274,11 @@ impl<T: Copy> TxState<T> {
                     fee::display(&fee, &mut self.processor);
                 }
             }
-            DetectedTxType::Other(fee) | DetectedTxType::Error(fee) => if let Some(fee) = fee {
-                fee::display(fee, &mut self.processor);
-            },
+            DetectedTxType::Other(fee) | DetectedTxType::Error(fee) => {
+                if let Some(fee) = fee {
+                    fee::display(fee, &mut self.processor);
+                }
+            }
         }
     }
 
